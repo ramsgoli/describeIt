a314455adbb023c50a355971e12f89ca92cce579
Ram Goli <ramsgoli@gmail.com>
2018-03-30 19:05:22 -0700

    render all other players in results

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -5,7 +5,6 @@ import Quote from 'components/Quote';
 class Results extends React.Component {
     renderWinners = () => {
         return this.props.results.get('winners').map(winner => {
-            const numCorrect = this.props.results.get(winner);
             let player;
 
             if (winner === this.props.currentPlayer.get('name')) {
@@ -15,6 +14,7 @@ class Results extends React.Component {
                     return player.get('name') == winner;
                 });
             }
+            const numCorrect = this.props.results.get('players').find(_player => _player.get('name') === player.get('name')).get('numCorrect');
 
             return (
                 <div className="winner">
@@ -30,12 +30,40 @@ class Results extends React.Component {
         })
     }
 
+    renderPlayers = () => {
+        console.log(this.props);
+        return this.props.results.get('players').map(player => {
+            if (this.props.results.get('winners').includes(player.get('name'))) {
+                return null;
+            } 
+            let playerObj = this.props.players.find(_player => {
+                return _player.get('name') == player.get('name');
+            });
+            playerObj = playerObj || this.props.currentPlayer;
+
+            return (
+                <div className="player">
+                    <h1>{player.get('name')} - guessed {player.get('numCorrect')} right</h1>
+                    <div className="quote-container">
+                        <Quote className="purple" />
+                        <div className="submission-text">
+                            {playerObj.getIn(['submission', 'text'])}
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+
     render() {
         return (
             <div className="results-container">
                 <div className="heading">Results</div>
                 <div className="winners">
                     {this.renderWinners()}
+                </div>
+                <div className="players">
+                    {this.renderPlayers()}
                 </div>
             </div>
         );
